5cc1c88aec5eef03af37fca367d68a200e60090c
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2021-07-06 15:48:58 +1200

    Bring the HTML renderer up to spec with Item API changes

--- exporters/HTML.py
+++ exporters/HTML.py
@@ -3,21 +3,21 @@ import extensions
 NAME = 'HTML'
 
 def export(app):
-    m = {'h1': 'h1', 'h2': 'h2', 'body': 'p', 'tex': 'math'}
+    m = {'body': 'p', 'tex': 'math'}
 
     imageCount = 0
 
     out = "<html>\n<body>\n"
     for i in app.items:
         # Get info from string
-        c, r = extensions.classify(i.string)
+        c, r = extensions.classify(i.get())
 
-        image, output = r.export(i, app.s.styles[c])
+        image, output = r.export(i, app.s.styles.get(c, app.s.styles.get('body', {})))
 
         if not image:
-            out += f"<{m[c]}>"
+            out += f"<{m.get(c, c)}>"
             out += output
-            out += f"</{m[c]}>"
+            out += f"</{m.get(c, c)}>"
             out += "\n"
         else:
             path = f"img/{imageCount}.png"
