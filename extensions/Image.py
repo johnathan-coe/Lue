c0ac712813d31ae4b5866e21a86df76d15b105f3
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2021-07-13 02:26:26 +1200

    Update Image extension for unified rendering API

--- extensions/Image.py
+++ extensions/Image.py
@@ -5,24 +5,11 @@ PREFIX = "^"
 # Called when a line starts with PREFIX
 # Return the styling class, Body string and rendering routine
 def lex(string):
-    return "img", ImageRenderer
+    return "img", render
 
-class ImageRenderer:
-    @staticmethod
-    def render(string, label, styles):
-        # Apply styling
-        label.configure(**styles)
-        
-        filename = string[1:]
-        try:
-            label.image = ImageTk.PhotoImage(Image.open(filename))
-        except FileNotFoundError:
-            label.configure(text=f"Image '{filename}' not found!")
-            return
-        
-        # Add image and specified styling from stylesheet
-        label.configure(image=label.image)
-
-    @staticmethod
-    def export(string, style):
-        return Image.open(string[1:])
+def render(string, styles):
+    filename = string[1:]
+    try:
+        return Image.open(filename)
+    except FileNotFoundError:
+        return f"Image '{filename}' not found!"
