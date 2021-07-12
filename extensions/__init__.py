580c99d4ab4552dd973d0ad96a892ebf6755c82b
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2021-07-13 02:31:39 +1200

    Update body and heading classification for new rendering API

--- extensions/__init__.py
+++ extensions/__init__.py
@@ -1,16 +1,16 @@
 import config
-from .PlainText import PlainText
+from . import PlainText
 
 def classify(string):
     if string.startswith('###'):
-        return "h3", PlainText
+        return "h3", PlainText.render
     elif string.startswith('##'):
-        return "h2", PlainText
+        return "h2", PlainText.render
     elif string.startswith('#'):
-        return "h1", PlainText
+        return "h1", PlainText.render
 
     for ext in config.EXTENSIONS:
         if string.startswith(ext.PREFIX):
             return ext.lex(string)        
 
-    return "body", PlainText
+    return "body", PlainText.render
