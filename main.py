b875af8f5d0931ef600d4621536e1a54b756cda1
Johnathan Coe <jcoe148@aucklanduni.ac.nz>
2021-07-23 10:24:47 +1200

    Inform the user in the menu when a save is performed

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from Viewer.Viewer import Viewer
 from tkinter import filedialog
 import os
 import sys
+from datetime import datetime
 
 FILETYPES = (
     ("Lue Files", "*.lue"),
@@ -38,6 +39,7 @@ class App(tk.Tk):
 
         self.itemFrame.pack(fill=tk.BOTH, expand=True)
         
+        self.menu = None
         self.attachMenuBar()
         self.mainloop()
 
@@ -52,41 +54,52 @@ class App(tk.Tk):
             self.itemFrame.items[0].edit()
             self.filename = filename
 
+    def updateSaveStatus(self, info):
+        self.menu.delete(tk.END)
+        self.menu.add_command(label=info)
+
     def save(self):
         if self.filename:
+            self.updateSaveStatus('Saving...')
             self.itemFrame.saveToFile(self.filename)
+            self.updateSaveStatus(f'Saved at {datetime.now().time()}')
         else:
             self.saveas()
 
     def saveas(self):
         if filename := filedialog.asksaveasfilename(filetypes=FILETYPES):
+            self.updateSaveStatus('Saving...')
             self.itemFrame.saveToFile(filename)
             self.filename = filename
+            self.updateSaveStatus(f'Saved at {datetime.now().time()}')
 
     def attachMenuBar(self):
         # Define a menu
-        menu = tk.Menu(self)
-        self.config(menu=menu)
+        self.menu = tk.Menu(self)
+        self.config(menu=self.menu)
         
         # Build the file menu
-        fileMenu = tk.Menu(menu, tearoff=0)
+        fileMenu = tk.Menu(self.menu, tearoff=0)
         fileCmds = {"New": self.new, "Open": self.open, "Save": self.save, "Save As": self.saveas}
         [fileMenu.add_command(label=label, command=command) for label, command in fileCmds.items()]
         # Add it to the top bar
-        menu.add_cascade(label="File", menu=fileMenu)
+        self.menu.add_cascade(label="File", menu=fileMenu)
 
         # Build a menu for available themes
-        themeMenu = tk.Menu(menu, tearoff=0)
+        themeMenu = tk.Menu(self.menu, tearoff=0)
         for theme in config.THEMEDIRS:
             themeMenu.add_command(label=theme.split('/')[-1],
                 command=lambda t=theme: self.itemFrame.style(themes.Theme(t)))
-        menu.add_cascade(label="Theme", menu=themeMenu)
+        self.menu.add_cascade(label="Theme", menu=themeMenu)
 
         # Build a menu for available exporters 
-        exportMenu = tk.Menu(menu, tearoff=0)
+        exportMenu = tk.Menu(self.menu, tearoff=0)
         for exporter in config.EXPORTERS:
             exportMenu.add_command(label=exporter.NAME, command=lambda x=exporter: x.export(self.itemFrame))
-        menu.add_cascade(label="Export", menu=exportMenu)
+        self.menu.add_cascade(label="Export", menu=exportMenu)
+
+        self.menu.add_separator()
+        self.menu.add_command(label="Unsaved since load!")
             
 if __name__ == "__main__":
     App()
